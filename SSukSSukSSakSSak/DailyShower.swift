b34cecb09168d70d4cb7d359de8bd5d6c244bd9a
abovocado <dkstjdud02@gmail.com>
2023-04-06 18:05:18 +0900

    DailyShower.swift_DailyShower Structure Created

---
+++ SSukSSukSSakSSak/DailyShower.swift
@@ -0,0 +1,17 @@
+//
+//  DailyShower.swift
+//  SSukSSukSSakSSak
+//
+//  Created by SY AN on 2023/04/06.
+//
+
+import Foundation
+
+struct DailyShower {
+    var title: String
+    var showerTime: Int
+    var bodies: [String]
+    
+    
+    
+}
