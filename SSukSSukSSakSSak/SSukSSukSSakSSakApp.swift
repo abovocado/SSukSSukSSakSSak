bcd55f88d916f3d785688fc873fcac8ce6d5d9b5
Lily <dkstjdud02@gmail.com>
2023-04-06 17:36:52 +0900

    Initial Commit

---
+++ SSukSSukSSakSSak/SSukSSukSSakSSakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSukSSukSSakSSakApp.swift
+//  SSukSSukSSakSSak
+//
+//  Created by SY AN on 2023/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SSukSSukSSakSSakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
