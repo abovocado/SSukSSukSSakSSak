eb3023ce9dc1d58785fa5e9a98f0f348daf0117b
abovocado <dkstjdud02@gmail.com>
2023-04-11 17:10:03 +0900

    SSukSSukSSakSSakApp_Root view changed to SSukSSukSSakSSakView.swift

--- SSukSSukSSakSSak/SSukSSukSSakSSakView.swift
+++ SSukSSukSSakSSak/SSukSSukSSakSSakView.swift
@@ -3,11 +3,12 @@
 //  SSukSSukSSakSSak
 //
 //  Created by SY AN on 2023/04/06.
-//
+// 앱의 첫 화면, List 형태로 DailyFight마다 가진 속성들을 뿌려줌
 
 import SwiftUI
 
 struct SSukSSukSSakSSakView: View {
+    // 필요한 파라미터는 우리가 보여줄 DailyShower 타입을 원소로 가지는 배열
     var showers: [DailyShower]
     var body: some View {
         List{
