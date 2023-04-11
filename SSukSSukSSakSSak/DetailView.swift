fd119d1b50c02c7ba016c246ed9cdb51523c479e
abovocado <dkstjdud02@gmail.com>
2023-04-11 17:48:51 +0900

    DetailView.swift_View for Showing Detail information about a Shower Instance

---
+++ SSukSSukSSakSSak/DetailView.swift
@@ -0,0 +1,55 @@
+//
+//  DetailView.swift
+//  SSukSSukSSakSSak
+//
+//  Created by SY AN on 2023/04/11.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var shower:DailyShower
+    
+    var body: some View {
+        VStack{
+            //Shower Info
+            
+            List{
+                Section(header:Text("Shower Info")){
+                    Label("Start Meeting", systemImage: "clock")
+                    HStack{
+                        Label("Length", systemImage: "timer")
+                        Spacer()
+                        Text("\(shower.showerTime) Minutes")
+                    }
+                    
+                    HStack{
+                        Label("Theme", systemImage: "paintpalette")
+                        Spacer()
+                        Text(shower.theme.rawValue)
+                            .padding(4)
+                            .background(shower.theme.mainColor)
+                            .foregroundColor(shower.theme.accentColor)
+                            .cornerRadius(4)
+                    }
+                }
+                
+                Section(header:Text("Bodies")){
+                    List{
+                       
+                    }
+                }
+                
+                Section(header:Text("History")){
+                    
+                }
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(shower:DailyShower.sampleData[0])
+    }
+}
